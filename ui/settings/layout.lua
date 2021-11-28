03854720242e64fbb1c5735eef15358b69a39f8c
nullium21 <rommyskovets@gmail.com>
2021-11-28 19:05:41 +0200

    Move settings buttons to their own vars

--- ui/settings/layout.lua
+++ ui/settings/layout.lua
@@ -1,6 +1,10 @@
 local button = require "ui.settings.button"
 
+local wifi_btn = button { text="", type="toggle" }
+
+local blth_btn = button { text="", type="toggle" }
+
 return {
-    { x=1,y=1, button { text="", type="toggle" } },
-    { x=3,y=1, button { text="", type="toggle" } },
+    { x=1,y=1, wifi_btn },
+    { x=3,y=1, blth_btn },
 }
